13495d5bafbfba5666f919a6271434308eab2178
sheeeng <sheeeng@2c58baca-6f79-9fc6-77d6-df0e744804b7>
2010-07-10 11:00:28 +0000

    Implemented reflection on popup.html using -webkit-box-reflect style. Implemented desktop notification.

--- trunk/GoogleDevFestCountdown/desktop_notifier.js
+++ trunk/GoogleDevFestCountdown/desktop_notifier.js
@@ -8,6 +8,8 @@
  * http://gecko.hp2.jp/chrome_notify/
  */
 
+var notifyCount = 0;
+ 
 function Notifier() {}
 
 // Returns "true" if this browser supports notifications.
@@ -30,12 +32,18 @@ Notifier.prototype.RequestPermission = function(cb) {
 // Popup a notification with icon, title, and body. Returns false if
 // permission was not granted.
 Notifier.prototype.Notify = function(icon, title, body) {
-  if (window.webkitNotifications.checkPermission() == 0) {
+  if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
 	var popup = window.webkitNotifications.createNotification(
 	icon, title, body);
+	if(notifyCount > 0)
+	{
+		return; //show only one notification at one time
+	}
+	notifyCount++;
 	popup.show();
 	setTimeout(function(){ //automatically close after 5 seconds
       popup.cancel();
+	  notifyCount--;
     },5000);
 	return true;
   }
